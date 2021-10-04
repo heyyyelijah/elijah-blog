7dfd33c970410fbf9394f688db0bdd94d3741e67
Cj Elijah Garay <elijahmamuri@gmail.com>
2021-10-03 18:30:19 -0700

    reverted secret key back to main.py to see if this is causing the app crash

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ import os
 
 Base = declarative_base()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+# app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro',
